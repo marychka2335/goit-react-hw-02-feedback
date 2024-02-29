5e24b80a065c1af259d7b361e5a6f1744a3324fd
marychka2335 <marychka2335@icloud.com>
2024-02-29 19:27:06 +0200

    second commit with description change

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -4,11 +4,8 @@ export const Description = () => {
   return (
     <div className={css.container}>
       <header>
-        <h1>Sip Happens Café</h1>
-        <p>
-          Please leave your feedback about our service by selecting one of the
-          options below.
-        </p>
+        <h1>Cafe Expresso</h1>
+        <p>Please leave feedback</p>
       </header>
     </div>
   );
